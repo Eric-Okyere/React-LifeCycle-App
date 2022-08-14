05c3866afa59426676fd4606764e9eeac4be9a59
Eric Okyere <102455856+Eric-Okyere@users.noreply.github.com>
2022-08-14 09:29:09 +0000

    Conmponents LifeCycle Exercise1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import UserClass from './components/UserClass';
+
+
+function App() {
+  return (
+    <div className="App">
+     <UserClass />
+    </div>
+  );
+}
+
+export default App;
